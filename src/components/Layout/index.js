1015a8654c0e837344144fba2ed4793f5c1a976e
Michele Fasciano <michelefasciano37@hotmail.co.uk>
2019-10-16 11:22:22 +0100

    Loading page update

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -11,15 +11,37 @@ import Footer from '../Footer/index'
 
 library.add(fab)
 
-function App() {  
-  return (
-    <>
-      <SEO />
-      <Header />
-      <Content />
-      <Footer />
-    </>
-  )
+class App extends React.Component {
+
+  // fake authentication Promise
+  authenticate(){
+    return new Promise(resolve => setTimeout(resolve, 2000)) // 2 seconds
+  }
+
+  componentDidMount(){
+    this.authenticate().then(() => {
+      const ele = document.getElementById('loader')
+      if(ele){
+        // fade out
+        ele.classList.add('available')
+        setTimeout(() => {
+          // remove from DOM
+          ele.outerHTML = ''
+        }, 2000)
+      }
+    })
+  }
+
+  render() { 
+    return (
+      <>
+        <SEO />
+        <Header />
+        <Content />
+        <Footer />
+      </>
+    )
+  }
 }
 
 export default App
