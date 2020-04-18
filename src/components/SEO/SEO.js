c0ad439fb7db7a0089b6f8f15af50bb0ed2525d8
Michele Fasciano <michelefasciano37@hotmail.co.uk>
2020-04-18 08:48:20 +0100

    Meta tags update

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -9,25 +9,25 @@ const SEO = () => {
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
-      <meta name="description" content="Front-end web developer based in London with passion for responsive websites." />
+      <meta name="description" content="Frontend web developer based in London with passion for responsive websites." />
       <link rel="apple-touch-icon" href="logo192.png" />
 
       <title>Michele Fasciano | Portfolio</title>
 
       {/* Open Graph tags */}
-      <meta property="og:url" content="https://m-fasciano.github.io/" />
+      <meta property="og:url" content="https://m-fasciano.github.io/portfolio/" />
       <meta property="og:type" content="webiste" />
       <meta property="og:title" content="Michele Fasciano | Portfolio" />
-      <meta property="og:description" content="Front-end web developer based in London with passion for responsive websites." />
+      <meta property="og:description" content="Frontend web developer based in London with passion for responsive websites." />
       <meta property="og:image" content={metaImg} />
 
       {/* Twitter tags */}
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@webmf" />
       <meta name="twitter:creator" content="@webmf" />
-      <meta property="og:url" content="https://m-fasciano.github.io/" />
+      <meta property="og:url" content="https://m-fasciano.github.io/portfolio/" />
       <meta property="og:title" content="Michele Fasciano | Portfolio" />
-      <meta property="og:description" content="Front-end web developer based in London with passion for responsive websites." />
+      <meta property="og:description" content="Frontend web developer based in London with passion for responsive websites." />
       <meta property="og:image" content={metaImg} />
 
       <link rel="shortcut icon" href={favicon} />
