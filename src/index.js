60f906854529152ea78f85b22c75bcdcd3208e5a
Michele Fasciano <michelefasciano37@hotmail.co.uk>
2020-04-25 08:40:07 +0100

    Service worker removed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import '@babel/polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/Layout/Layout'
-import * as serviceWorker from './serviceWorker'
+// import * as serviceWorker from './serviceWorker'
 // import ReactGA from 'react-ga'
 // ReactGA.initialize('UA-149866002-1')
 // ReactGA.pageview(window.location.pathname + window.location.search)
@@ -15,4 +15,4 @@ ReactDOM.render(<App />, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+// serviceWorker.register()
